f3dac2114884b5319d17ec887d7d0bed4b4eb82e
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-02-15 21:07:40 -0500

    Show labels in table

--- src/components/home/TableRow.vue
+++ src/components/home/TableRow.vue
@@ -1,11 +1,8 @@
 <template>
   <div
     :class="[
-      !email.is_read
-        ? 'font-bold dark:text-gray-300'
-        : 'text-gray-800 dark:text-gray-200',
       isEmailSelected ? 'bg-gray-100 dark:bg-gray-900/40' : '',
-      'grid grid-cols-[5%_25%_60%_10%] px-3 py-2 hover:shadow-lg cursor-pointer overflow-hidden',
+      'grid grid-cols-[5%_25%_60%_10%] px-3 py-2 hover:shadow-lg cursor-pointer overflow-hidden text-gray-800 dark:text-gray-200',
     ]"
   >
     <div class="flex h-6 items-center mr-3">
@@ -16,13 +13,41 @@
         class="h-4 w-4 rounded border-gray-300 dark:border-gray-900 dark:bg-gray-600 text-indigo-600 focus:ring-indigo-500"
       />
     </div>
-    <div class="whitespace-nowrap">
+    <div
+      :class="[
+        !email.is_read
+          ? 'font-bold dark:text-gray-300'
+          : 'text-gray-800 dark:text-gray-200',
+        'whitespace-nowrap',
+      ]"
+    >
       {{ email.sender.label }}
     </div>
-    <div class="flex gap-1 whitespace-nowrap">
-      <p>{{ email.subject }}</p>
+    <div class="flex gap-1 whitespace-nowrap overflow-hidden truncate">
+      <div class="flex gap-2">
+        <span
+          v-for="label in email.labels"
+          :key="label.id"
+          :class="[
+            getLabelColorClass(label),
+            'inline-flex items-center rounded px-2 py-0.5 text-xs font-medium',
+          ]"
+        >
+          {{ label.name }}
+        </span>
+      </div>
+
+      <p
+        :class="[
+          !email.is_read
+            ? 'font-bold dark:text-gray-300'
+            : 'text-gray-800 dark:text-gray-200',
+        ]"
+      >
+        {{ email.subject }}
+      </p>
       <p>-</p>
-      <p class="whitespace-nowrap overflow-hidden truncate">
+      <p class="whitespace-nowrap">
         {{ getBodyPreview }}
       </p>
     </div>
@@ -63,6 +88,52 @@ export default {
         useSelectedEmailStore().deselectEmail(this.email);
       }
     },
+    getLabelColorClass(label) {
+      switch (label.color) {
+        case "black":
+          return "bg-black text-white";
+        case "slate":
+          return "bg-slate-300 text-slate-800";
+        case "gray":
+          return "bg-gray-300 text-gray-800";
+        case "red":
+          return "bg-red-300 text-red-800";
+        case "orange":
+          return "bg-orange-300 text-orange-800";
+        case "amber":
+          return "bg-amber-300 text-amber-800";
+        case "yellow":
+          return "bg-yellow-300 text-yellow-800";
+        case "lime":
+          return "bg-lime-300 text-lime-800";
+        case "green":
+          return "bg-green-300 text-green-800";
+        case "emerald":
+          return "bg-emerald-300 text-emerald-800";
+        case "teal":
+          return "bg-teal-300 text-teal-800";
+        case "cyan":
+          return "bg-cyan-300 text-cyan-800";
+        case "sky":
+          return "bg-sky-300 text-sky-800";
+        case "blue":
+          return "bg-blue-300 text-blue-800";
+        case "indigo":
+          return "bg-indigo-300 text-indigo-800";
+        case "violet":
+          return "bg-violet-300 text-violet-800";
+        case "purple":
+          return "bg-purple-300 text-purple-800";
+        case "fuchsia":
+          return "bg-fuchsia-300 text-fuchsia-800";
+        case "pink":
+          return "bg-pink-300 text-pink-800";
+        case "rose":
+          return "bg-rose-300 text-rose-800";
+        default:
+          return "bg-gray-300 text-gray-800";
+      }
+    },
   },
   computed: {
     getBodyPreview() {
@@ -70,7 +141,7 @@ export default {
     },
     getTime() {
       const received = DateTime.fromFormat(
-        this.email.recieved_at,
+        this.email.received_at,
         "yyyy-MM-dd HH:mm:ss"
       );
       const now = DateTime.now();
